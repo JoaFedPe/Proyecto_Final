b23453fc1a1f30a7757c31faefc1b804e2a3b882
JoaFedPe <joafedpe@gmail.com>
2024-04-23 18:21:33 -0300

    Primera Pre-entrega

---
+++ src/routes/carts.router.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const fs = require("fs").promises;
+const fsync = require("fs");
+const router = express.Router();
+
+let carts = [];
+
+router.post("/carts", async (req, res) => {
+  const { products } = req.body;
+  if (!fsync.existsSync("carritos.json"))
+    fs.writeFile("carritos.json", JSON.stringify(carts, null, 2));
+  const data = await fs.readFile("carritos.json", "utf8");
+  carts = JSON.parse(data);
+  const CartId = carts.length + 1;
+  const newCart = {
+    CartId,
+    products: products ? products : [],
+  };
+  carts.push(newCart);
+
+  await fs.writeFile("carritos.json", JSON.stringify(carts, null, 2));
+  return res.json(newCart);
+});
+
+router.get("/carts/:cid", async (req, res) => {
+  const data = await fs.readFile("carritos.json", "utf8");
+  const carts = JSON.parse(data);
+  const cartId = parseInt(req.params.cid);
+  const cart = carts.find((cart) => cart.CartId === cartId);
+
+  if (cart) {
+    res.json(cart.products);
+  } else {
+    res.status(404).json({ msg: "carrito no encontrado" });
+  }
+});
+
+router.put("/carts/:cid/product/:pid", async (req, res) => {
+  const pdata = await fs.readFile("productos.json", "utf8");
+  let products = JSON.parse(pdata);
+
+  const cdata = await fs.readFile("carritos.json", "utf8");
+  let carts = JSON.parse(cdata);
+
+  const productId = parseInt(req.params.pid);
+  const cartId = parseInt(req.params.cid);
+
+  const product = products.find((product) => product.id === productId);
+  const cart = carts.find((cart) => cart.CartId === cartId);
+
+  const checkProduct = cart.products.find(
+    (product) => product.productId === productId
+  );
+
+  if (checkProduct) {
+    checkProduct.quantity++;
+  } else {
+    cart.products.push({ productId, quantity: 1 });
+  }
+
+  await fs.writeFile("carritos.json", JSON.stringify(carts, null, 2));
+
+  res.json({ msg: "producto agregado al carrito" });
+});
+
+module.exports = router;
---
+++ src/routes/products.router.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const fs = require("fs").promises;
+const fsync = require("fs");
+const router = express.Router();
+
+let products = [];
+
+router.post("/products", async (req, res) => {
+  const { title, description, code, price, status, stock, category } = req.body;
+  if (!fsync.existsSync("productos.json"))
+    fs.writeFile("productos.json", JSON.stringify(products, null, 2));
+  const data = await fs.readFile("productos.json", "utf8");
+  products = JSON.parse(data);
+  const id = products.length + 1;
+  const newProduct = {
+    id,
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+  };
+  products.push(newProduct);
+
+  await fs.writeFile("productos.json", JSON.stringify(products, null, 2));
+  return res.json(newProduct);
+});
+
+router.get("/products", async (req, res) => {
+  const data = await fs.readFile("productos.json", "utf8");
+  const products = JSON.parse(data);
+  const limit = req.query.limit;
+  if (limit) {
+    res.json(products.slice(0, limit));
+  } else {
+    res.json(products);
+  }
+  console.log(products);
+});
+
+router.get("/products/:pid", async (req, res) => {
+  const data = await fs.readFile("productos.json", "utf8");
+  const products = JSON.parse(data);
+  const productId = parseInt(req.params.pid);
+  const product = products.find((product) => product.id === productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ msg: "Producto no encontrado" });
+  }
+});
+
+router.put("/products/:pid", async (req, res) => {
+  const data = await fs.readFile("productos.json", "utf8");
+  const products = JSON.parse(data);
+  const productId = parseInt(req.params.pid);
+  const product = products.find((product) => product.id === productId);
+
+  if (product) {
+    const { title, description, code, price, stock, category } = req.body;
+    product.title = title;
+    product.description = description;
+    product.code = code;
+    product.price = price;
+    product.stock = stock;
+    product.category = category;
+
+    await fs.writeFile("productos.json", JSON.stringify(products, null, 2));
+    res.json(product);
+  } else {
+    res.status(404).json({ msg: "Producto no encontrado" });
+  }
+});
+
+router.delete("/products/:pid", async (req, res) => {
+  const data = await fs.readFile("productos.json", "utf8");
+  let products = JSON.parse(data);
+  const productId = parseInt(req.params.pid);
+  const product = products.filter((product) => product.id !== productId);
+  if (product) {
+    await fs.writeFile("productos.json", JSON.stringify(product, null, 2));
+    res.json(product);
+  }
+});
+
+module.exports = router;
